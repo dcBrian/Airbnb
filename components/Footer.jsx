1fe5a05164ce65ff78c43f442cd4fbd700bfd84d
dev <ivucady@gmail.com>
2022-08-19 01:06:02 +0900

    add home page

---
+++ components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 text-center md:grid-cols-4 gap-10 md:px-32 py-14 bg-gray-100 ">
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real website</p>
+        <p>It's a pretty awesome clone</p>
+        <p>Instant Book</p>
+        <p>Gift Cards</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>I am looking for </p>
+        <p>A cool job</p>
+        <p>Feel free to</p>
+        <p>Contact me if</p>
+        <p>You have an offer</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help center</p>
+        <p>24/24</p>
+        <p>Travel credits</p>
+        <p>Responsible Hosting</p>
+        <p>Terms & Privacy</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/MediumCard.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const MediumCard = ({ image, title }) => {
+  return (
+    <div className="cursor-pointer hover:scale-105 transition duration-300 ease-out">
+      <div className="relative h-80 w-80 ">
+        <Image className="rounded-xl" src={image} layout="fill" objectFit="contain" />
+      </div>
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+};
+
+export default MediumCard;
